4b75bcb7f5379f9cb664f63e0a041f64ce02f158
hortensia <824466875@qq.com>
2024-09-10 21:24:08 +0800

    add marisa & shoot cd

--- plane.rkt
+++ plane.rkt
@@ -6,6 +6,8 @@
 
 (define sprite (circle 5 "solid" "red"))
 
+(define player-sprite (bitmap "./marisa.png"))
+
 ; the *world* contains:
 ; - the position of player: posn
 ; - a list of position of enemies: posn list
@@ -15,11 +17,13 @@
 
 (define-struct projectile (emitter posn))
 
+(define-struct player (pos cd))
+
 (define-struct world (player enemy projectiles))
 
 (define start-state
   (make-world
-   (make-posn 0 0)
+   (make-player (make-posn 0 0) 0)
    (cons (make-posn 200 200) (cons (make-posn 100 100) '()))
    '()
    ))
@@ -41,73 +45,68 @@
       [else x])))
 
 ; constraint x in the world.
-(define confine-world-x (make-clamper 0 WIDTH))
+(define confine-pos-x (make-clamper 0 WIDTH))
 
-(check-expect (confine-world-x 10000) WIDTH)
-(check-expect (confine-world-x -11) 0)
+(check-expect (confine-pos-x 10000) WIDTH)
+(check-expect (confine-pos-x -11) 0)
 
 ; constraint y in the world
-(define confine-world-y (make-clamper 0 HEIGHT))
+(define confine-pos-y (make-clamper 0 HEIGHT))
 
-; move the sprite's x coordinate
-(define (move-sprite-horizontal how-to-move)
-  (λ (sprite)
-    (make-posn (how-to-move (posn-x sprite))
-               (posn-y sprite))))
+; move the pos's x coordinate
+(define (move-pos-horizontal how-to-move)
+  (λ (pos)
+    (make-posn (how-to-move (posn-x pos))
+               (posn-y pos))))
 
 (define (move-right x)
   (+ x speed))
 
 (define (confined-move-right x)
-  (confine-world-x (move-right x)))
+  (confine-pos-x (move-right x)))
 
-(define move-sprite-right
-  (move-sprite-horizontal confined-move-right))
+(define move-pos-right
+  (move-pos-horizontal confined-move-right))
 
 (define (move-left x) (- x speed))
 
-(define (confined-move-left x) (confine-world-x (move-left x)))
+(define (confined-move-left x) (confine-pos-x (move-left x)))
 
-(define move-sprite-left
-  (move-sprite-horizontal confined-move-left))
+(define move-pos-left
+  (move-pos-horizontal confined-move-left))
   
-; move the sprite's y coordinate
+; move pos's y coordinate
 
 (define down-speed (/ speed 2))
 
-(define (move-sprite-vertical how-to-move)
-  (λ (sprite)
-    (make-posn (posn-x sprite)
-               (how-to-move (posn-y sprite)))))
+(define (move-pos-vertical how-to-move)
+  (λ (pos)
+    (make-posn (posn-x pos)
+               (how-to-move (posn-y pos)))))
 
 (define (move-down x)
   (+ x down-speed))
 
 (define (confined-move-down x)
-  (confine-world-y (move-down x)))
+  (confine-pos-y (move-down x)))
 
-(define move-sprite-down (move-sprite-vertical confined-move-down))
+(define move-pos-down (move-pos-vertical confined-move-down))
 
 (define (move-up x) (- x speed))
 
-(define (confined-move-up x) (confine-world-y (move-up x)))
+(define (confined-move-up x) (confine-pos-y (move-up x)))
 
-(define move-sprite-up (move-sprite-vertical confined-move-up))
-
-(define (move-sprite sprite)
-  (move-sprite-right (move-sprite-down sprite)))
+(define move-pos-up (move-pos-vertical confined-move-up))
 
 ; move player in the world
 
 (define (move-player how-to-move world)
-  (make-world
-   (how-to-move (world-player world))
-   (world-enemy world)
-   (world-projectiles world)))
-
-(check-expect
- (move-sprite (make-posn 300 300))
- (make-posn 310 305))
+  (let ([player (world-player world)])
+    (make-world
+     (make-player (how-to-move (player-pos player))
+                  (player-cd player))
+     (world-enemy world)
+     (world-projectiles world))))
 
 (define BACKGROUND
   (empty-scene WIDTH HEIGHT))
@@ -115,22 +114,46 @@
 ; move the player on key event
 (define (alter-player-on-key world key)
   (cond
-    [(key=? key "w") (move-player move-sprite-up world)]
-    [(key=? key "s") (move-player move-sprite-down world)]
-    [(key=? key "a") (move-player move-sprite-left world)]
-    [(key=? key "d") (move-player move-sprite-right world)]
+    [(key=? key "w") (move-player move-pos-up world)]
+    [(key=? key "s") (move-player move-pos-down world)]
+    [(key=? key "a") (move-player move-pos-left world)]
+    [(key=? key "d") (move-player move-pos-right world)]
     [else world]))
 
-; on each tick, we let player emit one projectile, then let the projectile and enemy move.
+; on each tick, we let
+; 1) player emit one projectile, clear cd
+; 2) move the projectile and enemy.
+; 3) clear projectiles that touch the wall
+;
+(define (projectiles-tick player projectiles)
+  (let ([projectiles (cond
+          [(eq? (player-cd player) CD) (cons (player-pos player) projectiles)]
+          [else projectiles])])
+        (filter (λ (projectile)
+                  (not (eq? (posn-y projectile) 0)))
+                (map move-pos-up projectiles))))
+
+(define CD 10)
+
+(define (player-tick player)
+  (make-player (player-pos player)
+               (let ([cd (player-cd player)])
+                 (cond
+                   [(eq? cd 0) CD]
+                   [else (- cd 1)])))) 
+
 (define (world-tick world)
-  (make-world
-   (world-player world)
-   ; control the enemy on tick. always turn right
-   (map move-sprite-right (world-enemy world))
-   (cons (world-player world) (map move-sprite-up (world-projectiles world)))))
+  (let ([player (world-player world)]
+        [enemy  (world-enemy world)]
+        [projectiles (world-projectiles world)])
+    (make-world
+     (player-tick player)
+     ; control the enemy on tick. always turn right
+     (map move-pos-right enemy)
+     (projectiles-tick player projectiles))))
 
 ; render the sprite on our world.
-(define (place-sprite-at-pos x background)
+(define (place-sprite-at-pos-in sprite x background)
   (place-image sprite
                (posn-x x)
                (posn-y x)
@@ -144,13 +167,15 @@
 (define (place-entities-on entities bg)
   (fold entities bg
         (λ (enemy-pos bg)
-          (place-sprite-at-pos enemy-pos bg))))
+          (place-sprite-at-pos-in sprite enemy-pos bg))))
 
 ; render enemy and player in our world
+; TODO: refactor this
 (define (render world)
-  (place-sprite-at-pos (world-player world)
-                       (place-entities-on (world-enemy world)
-                                          (place-entities-on (world-projectiles world) BACKGROUND))))
+  (place-sprite-at-pos-in player-sprite
+                          (player-pos (world-player world))
+                          (place-entities-on (world-enemy world)
+                                             (place-entities-on (world-projectiles world) BACKGROUND))))
 
 (define (anyof x predicate)
   (cond
